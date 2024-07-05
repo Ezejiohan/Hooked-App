3e172f8ff74819dea2d1501cf73dccd69d34da1d
Ezejiohan <ezejiohan@gmail.com>
2024-07-05 16:32:15 +0100

    newly updated

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const express = require('express');
 const connectDB = require('./database/database');
 connectDB();
 
-
-const PORT = process.env.PORT || 4000;
 const app = express();
+const {notFound} = require('./middleware/not_found');
+const errorHandlerMiddleware = require('./middleware/errorhandler');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
+const PORT = process.env.PORT || 4000;
 
 app.listen(process.env.PORT, () => {
     console.log('app is listening on PORT ' + process.env.PORT)
