3528984dd0d460f597617f2aabb7067dd33a846e
Ezejiohan <ezejiohan@gmail.com>
2024-08-08 02:04:55 +0100

    new comment updated

--- models/admins.js
+++ models/admins.js
@@ -1,29 +1,39 @@
+// Import mongoose library for MongoDB interaction
 const mongoose = require('mongoose');
 
+// Define a schema for the 'Admin' model using mongoose.Schema
 const adminSchema = new mongoose.Schema({
+    // Define 'name' field with validation rules
     name: {
-        type: String,
-        required: [true, 'must provide a name'],
-        trim: true,
-        maxlength: [25, 'name must not be more than 20 characters']        
+        type: String, // Data type is String
+        required: [true, 'must provide a name'], // Field is required with a custom error message if not provided
+        trim: true, // Automatically trim whitespace from the beginning and end of the string
+        maxlength: [25, 'name must not be more than 20 characters'] // Maximum length of the name is 25 characters with a custom error message
     },
+    // Define 'email' field with validation rules
     email: {
-        type: String,
-        required: true,
-        unique: true
+        type: String, // Data type is String
+        required: true, // Field is required
+        unique: true // Ensures that each email is unique in the database
     },
+    // Define 'password' field with validation rules
     password: {
-        type: String,
-        required: [true, 'password must be 8 characters long and include at least one uppercase and one special character']
+        type: String, // Data type is String
+        required: [true, 'password must be 8 characters long and include at least one uppercase and one special character'] // Field is required with a custom error message
     },
+    // Define 'isVerified' field with a default value
     isVerified: {
-        type: Boolean,
-        default: false
+        type: Boolean, // Data type is Boolean
+        default: false // Default value is false, meaning admin accounts are not verified by default
     }
     
 }, { 
+    // Add timestamps to the schema, automatically adding 'createdAt' and 'updatedAt' fields
     timestamps: true 
 });
 
+// Create a Mongoose model named 'Admin' using the defined schema
 const Admin = mongoose.model('Admin', adminSchema);
-module.exports = Admin
+
+// Export the Admin model so it can be used in other parts of the application
+module.exports = Admin;
