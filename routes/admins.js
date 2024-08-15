62ea8ab6c54a26d00c869d559f989c30ea087df5
Ezejiohan <ezejiohan@gmail.com>
2024-08-15 20:04:09 +0100

    just updated

--- routes/admins.js
+++ routes/admins.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { signUp, login, verifyAdmin, adminChangePassword, adminForgotPassword } = require('../controllers/admin');
+const { signUp, login, verifyAdmin, adminChangePassword, adminForgotPassword, adminResetPassword } = require('../controllers/admin');
 const { authenticate } = require('../middleware/adminAuthentication');
 const adminRoute = express.Router();
 adminRoute.get('/', (req, res) => {
@@ -12,5 +12,6 @@ adminRoute.get('/admins/verifyAdmin/:id', (verifyAdmin));
 adminRoute.post('/admins/log_in', (login));
 adminRoute.put('/admins/changePassword/:id', authenticate, (adminChangePassword));
 adminRoute.post('/admins/forgotPassword', (adminForgotPassword));
+adminRoute.patch('admins/resetPassword/:id/:token', (adminResetPassword));
 
 module.exports = adminRoute;
